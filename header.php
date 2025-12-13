8c7220317e7e493aeb95ac1a747327c5f30b7a96
Jonathan Lewis <jonathan.lewis@welch.edu>
2025-12-13 12:27:53 -0600

    added the service section

--- header.php
+++ header.php
@@ -7,15 +7,20 @@
 </head>
 <body <?php body_class(); ?>>
 <header>
-    <div class="site-logo">
-    <?php
-    if ( function_exists('the_custom_logo') && has_custom_logo() ) {
-        the_custom_logo();
-    } else {
-        // Fallback: site title if no logo is set
-        echo '<h1><a href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a></h1>';
-    }
-    ?>
+    <div class="logo-info-wrapper">
+        <div class="site-logo">
+        <?php
+        if ( function_exists('the_custom_logo') && has_custom_logo() ) {
+            the_custom_logo();
+        } else {
+            // Fallback: site title if no logo is set
+            echo '<h1><a href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a></h1>';
+     }
+         ?>
+        </div>
+        <h2 class="church-name">
+    <?php echo esc_html( get_theme_mod('church_name', 'Our Church')); ?>
+    </h2>
 </div>
     <nav class="primary-menu">
         <?php
