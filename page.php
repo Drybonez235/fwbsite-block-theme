2db084b7116aff0cc7969f934e90b3804f88a5da
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-01-10 15:05:06 -0600

    added code and css for basic page that will be customized by a user

--- page.php
+++ page.php
@@ -1,3 +1,27 @@
 <?php get_header(); ?>
+<main id="main-content" class="site-main">
+    <?php
+    // Start The Loop
+    while ( have_posts() ) :
+        the_post();
+        ?>
 
+        <article id="post-<?php the_ID(); ?>" <?php post_class('content-container'); ?>>
+            
+            <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header>
+
+            <div class="entry-content">
+                <?php
+                the_content();
+                ?>
+            </div>
+
+        </article>
+
+        <?php
+    endwhile; // End of the loop.
+    ?>
+</main>
 <?php get_footer(); ?>
