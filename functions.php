427870f6916ab0a7ee41499e6eb409f1e1237562
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-01-10 15:11:54 -0600

    added code and css for basic page that will be customized by a user

--- functions.php
+++ functions.php
@@ -37,3 +37,12 @@ function mychurch_setup() {
     ]);
 }
 add_action('after_setup_theme', 'mychurch_setup');
+
+
+function my_theme_setup() {
+    // Adds support for editor styles and default block styles
+    add_theme_support( 'wp-block-styles' );
+    
+    // Adds support for responsive embedded content (like YouTube)
+    add_theme_support( 'responsive-embeds' );
+}
